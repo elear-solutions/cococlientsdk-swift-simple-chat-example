b3d7cd41675fb3011090f3e055f901ad757be292
Rohan Sahay <rohansahay@elear.solutions>
2021-01-16 11:31:09 +0530

    Initial Commit

---
+++ CocoSync/CocoSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoSyncApp.swift
+//  CocoSync
+//
+//  Created by rohan-elear on 16/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CocoSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocoSync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CocoSync
+//
+//  Created by rohan-elear on 16/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
